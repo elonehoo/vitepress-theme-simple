96ad9c8e213a5d2b4f5bac764223ba521fa2d257
xiaoxunyao <huchengyea@163.com>
2022-04-15 10:34:43 +0800

    chore: init `Me.vue`

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="antialiased">
+    <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <nav class="flex justify-between items-center py-10 font-bold">
+        <a class="text-xl" href="/" aria-label="The Elone Hoo Blog">
+          <img
+            class="inline-block mr-2"
+            style="width: 36px; height: 31px"
+            alt="logo"
+            src="../../public/logo.svg"
+          />
+          <span v-if="isIndex === 'other'" class="hidden md:inline">Elone Hoo</span>
+        </a>
+        <div class="text-sm text-gray-500 leading-5">
+          <a
+            class="hover:text-gray-700"
+            href="/"
+            rel="noopener"
+            >me </a
+          >
+          <span class="mr-2 ml-2">·</span>
+          <a class="hover:text-gray-700" href="/blog.html">blog</a>
+          <span class="mr-2 ml-2">·</span>
+          <a class="hover:text-gray-700" href="/projects.html">projects </a>
+          <span class="mr-2 ml-2">·</span>
+          <a
+            class="hover:text-gray-700"
+            href="https://github.com/elonehoo"
+            target="_blank"
+            rel="noopener"
+            >GitHub →</a
+          >
+        </div>
+      </nav>
+    </div>
+    <main class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <Me v-if="isIndex === 'me'" />
+      <!-- <Home v-else-if="isIndex === 'blog'" /> -->
+      <!-- <Projects v-else-if="isIndex === 'projects'" /> -->
+      <!-- <Article v-else /> -->
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { computed,ref } from 'vue'
+import { useRoute } from 'vitepress'
+import Me from "./Me.vue"
+// import Home from './Home.vue'
+// import Projects from "./Projects.vue"
+// import Article from './Article.vue'
+
+const route = useRoute()
+const isIndex = computed(() =>
+  route.path.replace(/index.html$/, '')==='/' ? 'me' :
+  route.path.replace(/index.html$/, '') === '/blog.html' ? 'blog' :
+  route.path.replace(/index.html$/, '') === '/projects.html' ? 'projects' :
+  'other'
+
+)
+</script>
---
+++ .vitepress/theme/Me.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="body">
+    <h1>Hello.</h1>
+    <p style="font-weight: 500; font-size: 2.2em">My name is {{$frontmatter.englishName}}.</p>
+    <p>
+      I am an student programmer and the founder of
+      <a :href="$frontmatter.organizeLink" target="_blank">{{$frontmatter.organizeName}}</a>  Open Source Community. Most of my work is open source and publicly available on
+      <a :href="$frontmatter.githubLink" target="_blank">GitHub</a>.
+    </p>
+    <p>
+        If you happen to speak Chinese, my Chinese name is {{$frontmatter.chineseName}} ({{$frontmatter.chinesePinyin}}).
+    </p>
+    <p>
+        Outside of programming and finish school and get a job, I enjoy video games.
+    </p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  .body {
+        font-family: 'Rubik', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+          Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+          sans-serif;
+        font-size: calc(10px + 0.33vw);
+        -webkit-font-smoothing: antialiased;
+        padding: 5vh 10vw;
+        color: #121314;
+  }
+  h1 {
+        font-size: 4.5em;
+        font-weight: 500;
+        margin-bottom: 0;
+  }
+  p {
+        font-size: 1.6em;
+        font-weight: 300;
+        line-height: 1.4;
+        max-width: 28em;
+  }
+  a {
+        text-decoration: none;
+        color: #121314;
+        position: relative;
+  }
+  a:after {
+        content: '';
+        position: absolute;
+        z-index: -1;
+        top: 60%;
+        left: -0.1em;
+        right: -0.1em;
+        bottom: 0;
+        transition: top 200ms cubic-bezier(0, 0.8, 0.13, 1);
+        background-color: rgba(79, 192, 141, 0.5);
+  }
+  a:hover:after {
+        top: 0%;
+  }
+</style>
