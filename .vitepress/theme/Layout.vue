635ab8db4fb1a33c2e2218a576ad5e755aed2fa1
xiaoxunyao <huchengyea@163.com>
2022-04-21 15:08:06 +0800

    fix: upadte config

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -2,20 +2,19 @@
   <div class="antialiased">
     <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <nav class="flex justify-between items-center py-10 font-bold">
-        <a class="text-xl" href="/" aria-label="The Simple Blog Blog">
+        <a class="text-xl" href="/" :aria-label="Layout.iconTitle">
           <img
             class="inline-block mr-2"
             style="width: 36px; height: 31px"
             alt="logo"
             src="../../public/logo.svg"
           />
-          <span v-if="isIndex === 'other'" class="hidden md:inline">Simple Blog</span>
+          <span v-if="isIndex === 'other'" class="hidden md:inline">{{Layout.homeTitle}}</span>
         </a>
         <div class="text-sm text-gray-500 leading-5">
           <a
             class="hover:text-gray-700"
             href="/"
-            rel="noopener"
             >me </a
           >
           <span class="mr-2 ml-2">·</span>
@@ -25,7 +24,7 @@
           <span class="mr-2 ml-2">·</span>
           <a
             class="hover:text-gray-700"
-            href="https://github.com/elonehoo"
+            :href="Layout.GitHubLink"
             target="_blank"
             rel="noopener"
             >GitHub →</a
@@ -45,10 +44,13 @@
 <script setup>
 import { computed,ref } from 'vue'
 import { useRoute } from 'vitepress'
+import {Layout} from '../../config.json'
 import Me from "./Me.vue"
 import Home from './Home.vue'
 import Projects from "./Projects.vue"
 import Article from './Article.vue'
+
+console.log(Layout)
 
 const route = useRoute()
 const isIndex = computed(() =>
--- index.md
+++ index.md
@@ -8,3 +8,51 @@ languageName: 简单博客
 languagePronunciation: Jiǎn dān bó kè
 title: About
 ---
+
+  <h1>Hello.</h1>
+  <p style="font-weight: 500; font-size: 2.2em">My name is {{$frontmatter.englishName}}.</p>
+  <p>
+    I am an student programmer and the founder of
+    <a :href="$frontmatter.organizeLink" target="_blank">{{$frontmatter.organizeName}}</a>  Open Source Community. Most of my work is open source and publicly available on
+    <a :href="$frontmatter.githubLink" target="_blank">GitHub</a>.
+  </p>
+  <p>
+    If you happen to speak {{$frontmatter.speakLanguage}}, my {{$frontmatter.speakLanguage}} name is {{$frontmatter.languageName}} ({{$frontmatter.languagePronunciation}}).
+  </p>
+  <p>
+    Outside of programming and finish school and get a job, I enjoy video games.
+  </p>
+
+
+<style scoped>
+  h1 {
+        font-size: 4.5em;
+        font-weight: 500;
+        margin-bottom: 0;
+  }
+  p {
+        font-size: 1.6em;
+        font-weight: 300;
+        line-height: 1.4;
+        max-width: 28em;
+  }
+  a {
+        text-decoration: none;
+        color: #121314;
+        position: relative;
+  }
+  a:after {
+        content: '';
+        position: absolute;
+        z-index: -1;
+        top: 60%;
+        left: -0.1em;
+        right: -0.1em;
+        bottom: 0;
+        transition: top 200ms cubic-bezier(0, 0.8, 0.13, 1);
+        background-color: rgba(79, 192, 141, 0.5);
+  }
+  a:hover:after {
+        top: 0%;
+  }
+</style>
